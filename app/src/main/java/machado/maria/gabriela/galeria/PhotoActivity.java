f41fab4816ccf9c35665aa85aadd13e778ac2f35
20221tiimi0297 <xucagabi22@gmail.com>
2024-06-04 10:49:38 -0300

    passo 6

--- app/src/main/java/machado/maria/gabriela/galeria/PhotoActivity.java
+++ app/src/main/java/machado/maria/gabriela/galeria/PhotoActivity.java
@@ -3,7 +3,9 @@ package machado.maria.gabriela.galeria;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +16,15 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        //seta tbPhoto como ActionBar padrao
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+
+        //coloca botao de voltar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_photo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
