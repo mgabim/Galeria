caeb85311ac0ca34c85c8b2dc4d0c1970e00b4fb
20221tiimi0297 <xucagabi22@gmail.com>
2024-06-18 11:12:28 -0300

    passo 9

--- app/src/main/java/machado/maria/gabriela/galeria/MainAdapter.java
+++ app/src/main/java/machado/maria/gabriela/galeria/MainAdapter.java
@@ -1,6 +1,7 @@
 package machado.maria.gabriela.galeria;
 
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,22 +21,26 @@ public class MainAdapter extends RecyclerView.Adapter {
         this.photos = photos;
     }
 
-    //@NonNull
-    //@Override
-    //public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    //    return null;
-    //}
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //cria os elementos de interface pra um item
+        LayoutInflater inflater = LayoutInflater.from(mainActivity); //cria elemento da interface
+        View v = inflater.inflate(R.layout.list_item, parent, false);
+        return new MyViewHolder(v);
 
-    //@Override
-    //public int getItemCount() {
-    //    return 0;
-    //}
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position){
         ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
         int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
         int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
-        Bitmap bitmap = Utils.getBitmap(photos.get(position), w, h);
+        Bitmap bitmap = Util.getBitmap(photos.get(position), w, h);
         imPhoto.setImageBitmap(bitmap);
         imPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
